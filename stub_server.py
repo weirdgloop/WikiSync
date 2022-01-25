d3133e8cffc070d3437f37434ab8314c8347945b
Andrew McAdams <andmcadams@gmail.com>
2022-01-24 16:38:49 -0800

    Update stub server to handle some more interesting cases involving timeouts

--- stub_server.py
+++ stub_server.py
@@ -1,7 +1,9 @@
 from wsgiref.simple_server import make_server
 
 import falcon
+import time
 
+counter = 0
 
 class TestResource:
 
@@ -9,20 +11,27 @@ class TestResource:
         """Handles GET requests"""
         # 4101 is the first prayer, toggle it to send data to the server
         resp.media = {
-            'varbits': [0, 100, 9657, 4101, 5000, 10000, 4104],
+            'varbits': [0, 100, 9657, 4101, 4102, 4103, 5000, 10000, 4104],
             'varps': [1, 3, 5, 6, 7, 10],
-            'version': 4
+            'version': 5
         }
         resp.status = falcon.HTTP_200
         return resp
 
     def on_post(self, req, resp):
+        global counter
         print(req.media)
-        resp.status = falcon.HTTP_200
+        if counter % 2 == 0:
+            resp.status = falcon.HTTP_200
+        else:
+            time.sleep(6)
+            resp.status = falcon.HTTP_400
+        counter += 1
+        return resp
 
     def on_get_check(self, req, resp):
         resp.media = {
-            'version': 4
+            'version': 5
         }
         resp.status = falcon.HTTP_200
         return resp
@@ -33,7 +42,7 @@ def create_app():
     # Resources are represented by long-lived class instances
     t = TestResource()
     app.add_route('/manifest', t)
-    app.add_route('/check_manifest', t, suffix='check')
+    app.add_route('/version', t, suffix='check')
     app.add_route('/submit', t)
     return app
 
