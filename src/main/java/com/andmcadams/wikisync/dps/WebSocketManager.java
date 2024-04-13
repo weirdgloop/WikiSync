7360688182ee066ceee8165485957dadb411093e
Zander Bolgar <lezed1@gmail.com>
2024-04-12 20:22:41 -0400

    Update Java-Websockets library to 1.5.6

--- src/main/java/com/andmcadams/wikisync/dps/WebSocketManager.java
+++ src/main/java/com/andmcadams/wikisync/dps/WebSocketManager.java
@@ -8,7 +8,6 @@ import com.andmcadams.wikisync.dps.ws.WSWebsocketServer;
 import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Objects;
@@ -169,7 +168,7 @@ public class WebSocketManager implements WSHandler
 				{
 					this.server.stop();
 				}
-				catch (InterruptedException | IOException e)
+				catch (InterruptedException e)
 				{
 					// ignored
 				}
